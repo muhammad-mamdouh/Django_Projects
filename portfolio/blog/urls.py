d4f57c86c6ca939e6df1ff3215b238422168cf43
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-28 23:41:20 +0200

    feature: Add route for viewing a specific blog post     - Add url at the blog urlpatterns for the route.     - Create a template for viewing post details.     - Create a corresponding post-details view function.

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import posts_list
+from .views import posts_list, post_detail
 
 urlpatterns = [
-    path('', posts_list, name='all_posts')
+    path('', posts_list, name='all_posts'),
+    path('<int:blog_id>/', post_detail, name='detailed_post')
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'posts': posts})
+
+
+def post_detail(request, blog_id):
+    post_details = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/post_details.html', {'post': post_details})
