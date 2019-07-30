589560e9fc08451631b0fabdc34dcfd1361d3871
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-30 05:15:49 +0200

    feature: Add a new home route for the project.     - Create the view function within the products app that       handles that route.     - Create the templates that extends the base one.     - Add a url within the project urlpatterns for that route.

--- producthunt/producthunt/urls.py
+++ producthunt/producthunt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views as products_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products_views.home, name='home')
 ]
