e9e59db1c2fcdac47399d8414825884c3b11a6ba
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-31 20:12:13 +0200

    feature: Create a product database model.     - Register the product model at the project admin panel.

--- producthunt/products/models.py
+++ producthunt/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    date_published = models.DateTimeField()
+    total_votes = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='icons/')
+    body = models.TextField()
+    url = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def date_published_formatted(self):
+        return f'{self.date_published.strftime("%b %-d, %Y")}'
+
+    def summary(self):
+        return f'{self.body[:250]} ...'
