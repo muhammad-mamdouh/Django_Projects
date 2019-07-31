9f30ec8bee21467095ddaaf80fa02026c280cad0
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-07-31 16:57:06 +0200

    feature: Create sign up page.     - Add a route at the project urlpatterns for the accounts urls' page.     - Add a route at the accounts urlpatterns for the sign up page.     - Add a sign up page within the accounts templates.     - Add a sign up view function that fullfills the sign up functionality.

--- producthunt/accounts/views.py
+++ producthunt/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['user_password']
+        confirmed_password = request.POST['user_confirm_password']
+        if password == confirmed_password:
+            try:
+                User.objects.get(username=username)
+                return render(request, 'accounts/signup.html',
+                              {'username_error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=username, password=password)
+                auth.login(request, user)
+                messages.success(request, f"Account created for {username}!")
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html',
+                          {'confirm_password_error': "Password doesn't match"})
+    else:
+        return render(request, 'accounts/signup.html')
--- producthunt/producthunt/urls.py
+++ producthunt/producthunt/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products import views as products_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products_views.home, name='home')
+    path('', products_views.home, name='home'),
+    path('accounts/', include('accounts.urls'))
 ]
