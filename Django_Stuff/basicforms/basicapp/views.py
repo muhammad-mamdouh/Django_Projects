0e5570f89474e945a5ae20a2270b49c1212c7aa5
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-05 11:31:19 +0200

    feature: Validate the form submission against bots

--- Django_Stuff/basicforms/basicapp/views.py
+++ Django_Stuff/basicforms/basicapp/views.py
@@ -8,4 +8,11 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print('VALIDATION SUCCESS!')
+            print(f'Name: {form.cleaned_data["name"]}')
+            print(f'Email: {form.cleaned_data["email"]}')
+            print(f'Text: {form.cleaned_data["text"]}')
     return render(request, 'basicapp/form_page.html', {'form': form})
