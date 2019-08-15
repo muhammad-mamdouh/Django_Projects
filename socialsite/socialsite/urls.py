9c68709b8aa3bcf657c5dd5802bd051fd68478fe
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-15 10:42:54 +0200

    feature: Add the home page of the site.     - Create a base template and an index template page.     - Create HomePage-view class that renders the index page.     - Add a home route to the home view.

--- socialsite/socialsite/urls.py
+++ socialsite/socialsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views as site_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', site_views.HomePage.as_view(), name='home'),
 ]
