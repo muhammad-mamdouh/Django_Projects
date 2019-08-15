66c6d2b2f7344ae1c24e33e35c3a367da734dd90
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-15 22:34:00 +0200

    feature: Create Subreddit and SubredditMembers models at the subreddits application

--- socialsite/subreddits/models.py
+++ socialsite/subreddits/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
+from django import template
+import misaka
 
-# Create your models here.
+register = template.Library()
+
+
+class Subreddit(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, blank=True, default='')
+    members = models.ManyToManyField(User, through='SubredditMembers')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('subreddits:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class SubredditMembers(models.Model):
+    subreddit = models.ForeignKey(Subreddit, related_name='memberships', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_subreddits', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('subreddit', 'user')
