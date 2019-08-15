1b4af6c6615bff4afa9eb02f81f6ea315d739448
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-15 15:46:17 +0200

    feature: Change the string representation of the User model and migrate it again

--- socialsite/accounts/models.py
+++ socialsite/accounts/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth import models
 
-# Create your models here.
+
+class User(models.User, models.PermissionsMixin):
+
+    def __str__(self):
+        return f'@{self.username}'
