d604809d94e61ae25bf11e183405b7667c979bfb
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-17 19:01:15 +0200

    fix: Fix the inline SubredditMembersInline class       that will be shown at the admin panel as a child for Subreddit model.

--- socialsite/subreddits/admin.py
+++ socialsite/subreddits/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import  Subreddit, SubredditMembers
+from .models import Subreddit, SubredditMembers
 
 
 class SubredditMembersInline(admin.TabularInline):
     model = SubredditMembers
 
 
-admin.site.register(Subreddit)
+class SubredditAdmin(admin.ModelAdmin):
+    inlines = [
+        SubredditMembersInline,
+    ]
+
+
+admin.site.register(Subreddit, SubredditAdmin)
 admin.site.register(SubredditMembers)
