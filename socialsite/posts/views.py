aab901ca3517f0fdbeaa07d2865d5741b4f4dd8b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-18 15:47:32 +0200

    feature: List posts for a specific user.     - Create posts-per-user template page.     - Create user-posts-view class.     - Add posts-for-user route to the posts application's urlpatterns.

--- socialsite/posts/views.py
+++ socialsite/posts/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView, CreateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import SelectRelatedMixin
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.http import Http404
 from .models import Post
 
 
@@ -44,3 +46,21 @@ class PostDetail(SelectRelatedMixin, DetailView):
     def get_queryset(self):
         queryset = super().queryset()
         return queryset.filter(user__username__iexact=self.kwargs.get('username'))
+
+
+class UserPosts(ListView):
+    model = Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user'] = self.post_user
+        return context
