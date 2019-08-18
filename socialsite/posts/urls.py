09b3bd6a90bd8a100caf134de802d631a5152b2d
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-18 12:16:19 +0200

    feature: Add the create post feature.     - Create the post create-form template page.     - Create the post-create-view class that renders that post creation form.     - Add a route to the posts application's urlpatterns for any new ones.

--- socialsite/posts/urls.py
+++ socialsite/posts/urls.py
@@ -5,4 +5,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='all'),
+    path('new/', views.CreatePost.as_view(), name='create'),
 ]
--- socialsite/posts/views.py
+++ socialsite/posts/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import SelectRelatedMixin
 from .models import Post
 
@@ -6,3 +7,15 @@ from .models import Post
 class PostList(SelectRelatedMixin, ListView):
     model = Post
     select_related = ('user', 'subreddit')
+
+
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, CreateView):
+    model = Post
+    select_related = ('user', 'subreddit')
+    fields = ['message', 'subreddit']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
