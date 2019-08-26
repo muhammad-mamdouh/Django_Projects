4f3de24c24a1efc09ae41d286237aeb4adb32c24
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-26 22:15:16 +0200

    feature: Return all the songs.     - Create a song listing class that returns a serialized format of all songs.     - Create a corresponding route and include the music app's urls into the project's one.

--- music_service/api/music/views.py
+++ music_service/api/music/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Songs
+from .serializers import SongsSerializer
 
-# Create your views here.
+
+class ListSongsView(generics.ListAPIView):
+    """
+    API endpoint that lists all of the current songs.
+    Provides a get method handler.
+    """
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
