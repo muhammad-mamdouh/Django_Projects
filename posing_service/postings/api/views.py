eb337ba150f40f1cf505716def29865a6c6c4df4
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-27 23:37:17 +0200

    feature: Add a search for blog-post feature

--- posing_service/postings/api/views.py
+++ posing_service/postings/api/views.py
@@ -1,11 +1,18 @@
+from django.db.models import Q
 from rest_framework import generics, mixins
 from postings.models import BlogPost
 from .serializers import BlogPostSerializer
 
 
 class BlogPostAPIView(mixins.CreateModelMixin , generics.ListAPIView):
-    queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    def get_queryset(self):
+        queryset = BlogPost.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            queryset = queryset.filter(Q(title__icontains=query)|Q(content__icontains=query)).distinct()
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
