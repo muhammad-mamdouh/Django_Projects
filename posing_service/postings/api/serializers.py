49d5baf37daad31d19eaa9dba241b30c3a5fc15b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-27 12:43:05 +0200

    feature: Add the read_only_fields list and validation for the title field.

--- posing_service/postings/api/serializers.py
+++ posing_service/postings/api/serializers.py
@@ -8,7 +8,7 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = '__all__'
-
+        read_only_fields = ['user']
         # fields = [
         #     'pk',
         #     'user',
@@ -16,3 +16,13 @@ class BlogPostSerializer(serializers.ModelSerializer):
         #     'content',
         #     'timestamp',
         # ]
+
+    def validate_title(self, value):
+        qs = BlogPost.objects.filter(title__iexact=value)
+        # Exclude the current retrieved instance
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+
+        if qs.exists():
+            raise serializers.ValidationError('This title has already been used')
+        return value
