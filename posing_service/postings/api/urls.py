13e28d95535be89b0d08b7e2c54c247f4826cae0
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-08-27 21:47:06 +0200

    feature: Add the create blog-post feature.     - Add a blog-post-api create view for creating a post.     - Add a corresponding route for it at the postings application.

--- posing_service/postings/api/urls.py
+++ posing_service/postings/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogPostRUDView
+from .views import BlogPostRUDView, BlogPostAPIView
 
 app_name = 'api-postings'
 urlpatterns = [
+    path('', BlogPostAPIView.as_view(), name='post-create'),
     path('<int:pk>/', BlogPostRUDView.as_view(), name='post-rud'),
 ]
--- posing_service/postings/api/views.py
+++ posing_service/postings/api/views.py
@@ -3,6 +3,14 @@ from postings.models import BlogPost
 from .serializers import BlogPostSerializer
 
 
+class BlogPostAPIView(generics.CreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class BlogPostRUDView(generics.RetrieveUpdateDestroyAPIView):
     # What field inside of our model are we looking up for?
     lookup_field = 'pk'    # Default value is the pk
