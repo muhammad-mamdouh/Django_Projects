61ea93cc5869641fbfcf4b68fbc6ce95526a8cfb
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 04:21:59 +0200

    feature: Add a login feature.     - Create an account-auth-form to view the login form.     - Create a login template page.     - Create a login view function to render login form at the login page and handle them.     - Add a login route to the account application.

--- src/account/urls.py
+++ src/account/urls.py
@@ -5,4 +5,5 @@ app_name    = 'account'
 urlpatterns = [
     path('register/', views.registration_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
 ]
--- src/account/views.py
+++ src/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .forms import RegistrationForm
+from .forms import RegistrationForm, AccountAuthenticationForm
 
 
 def registration_view(request):
@@ -20,6 +20,28 @@ def registration_view(request):
         form                         = RegistrationForm()
         context['registration_form'] = form
     return render(request, 'account/register.html', context)
+
+def login_view(request):
+    context = {}
+
+    user    = request.user
+    if user.is_authenticated:
+        return redirect('personal:home-page')
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email    = request.POST['email']
+            password = request.POST['password']
+            user     = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('personal:home-page')
+    else:   # GET request
+        form = AccountAuthenticationForm()
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
 
 def logout_view(request):
     logout(request)
