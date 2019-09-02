dbf9b9c36f6098b38475b99f05a28808c780c1df
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 15:26:27 +0200

    feature: Add password change feature.     - Add an EMAIL_BACKEND handler at the project's settings file.     - Add two routes to handle that feature, using the built in auth view.     - Add two corresponding template pages for the password change and confirmation functions.

--- src/myservice/urls.py
+++ src/myservice/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('personal.urls', namespace='personal')),
     path('', include('account.urls', namespace='account')),
+
+    # Password Reset Links
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'
+    ),
 ]
