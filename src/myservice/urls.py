3f382d6a5c94102065dbe89cc21758bb8a21cf40
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 15:39:05 +0200

    feature: Add the password reset using the email.     - Add the password reset routes following the django docs.     - And their corresponding template pages.

--- src/myservice/urls.py
+++ src/myservice/urls.py
@@ -33,4 +33,23 @@ urlpatterns = [
          auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
          name='password_change_done'
     ),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+        name='password_reset_complete'
+    ),
 ]
