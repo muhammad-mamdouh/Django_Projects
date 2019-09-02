afdf5a559b27d26d1744bde85077162ad0a78042
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 06:22:04 +0200

    fix: Reflect the changes to the form immedialtely

--- src/account/views.py
+++ src/account/views.py
@@ -56,7 +56,12 @@ def account_view(request):
     if request.POST:
         form = AccountUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
+            form.initial= {
+                'email': request.POST['email'],
+                'username': request.POST['username'],
+            }
             form.save()
+            context['success_message'] = 'Account updated successfully'
     else:   # GET request
         form = AccountUpdateForm(
             initial={
