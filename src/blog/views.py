8d42f8d122e287984e640b42a0c5364834f14a4a
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 18:37:17 +0200

    feature: Update the create posts feature and add a create form to it.     - Add create-blog-post form.     - Update the create_post template page with the create-blog-post form.     - Update the create-blog view function with the logic that handles the creation process.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
+from .forms import CreateBlogPostForm
 from .models import BlogPost
+from account.models import Account
 
 
+# @login_required
 def create_blog_view(request):
-    return render(request, 'blog/create_blog.html')
+    context = {}
+
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('must_authenticate')
+
+    form = CreateBlogPostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        obj        = form.save(commit=False)
+        author     = Account.objects.filter(email=user.email).first()
+        obj.author = author
+        obj.save()
+        return redirect('personal:home-page')
+
+    context['form'] = form
+    return render(request, 'blog/create_blog.html', context)
