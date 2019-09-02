b1f40176ee4055b8c1955f06cf99af7b60a5c33b
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-02 17:04:04 +0200

    feature: Add settings for the media and static files.     - Add routes at the project's main urlpatterns to the static and media dirs.

--- src/myservice/settings.py
+++ src/myservice/settings.py
@@ -128,4 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL       = '/static/'
+STATIC_ROOT      = os.path.join(BASE_DIR, 'static_CDN')
+MEDIA_URL        = '/media/'
+MEDIA_ROOT       = os.path.join(BASE_DIR, 'media_CDN')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
