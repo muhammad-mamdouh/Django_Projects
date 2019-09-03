18ed824be35fa7c2b19409fc07c81b8fd765a074
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-03 07:08:42 +0200

    feature: Add the CUD operations to the blog api.     - Create CUD view functions.     - Add routes for them at the blog-api urlpatterns.

--- src/blog/api/urls.py
+++ src/blog/api/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name    = 'blog'
 urlpatterns = [
     path('<slug:slug>/', views.api_detail_blog_view, name='detail'),
+    path('create', views.api_create_blog_view , name='create'),
+    path('<slug:slug>/update/', views.api_update_blog_view , name='update'),
+    path('<slug:slug>/delete/', views.api_delete_blog_view , name='delete'),
 ]
