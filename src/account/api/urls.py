b881c77951f1a116382ecd62b47a4cb2a4ae9b43
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-03 08:21:51 +0200

    feature: Add a login through the account api endpoint feature

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
 app_name    = 'account'
 urlpatterns = [
     path('register', views.registration_view, name='register'),
+    path('login', obtain_auth_token, name='login'),
 ]
