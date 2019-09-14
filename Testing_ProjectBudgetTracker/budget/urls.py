ea13ac4c213df42e32b639523b402589941c1d7f
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-15 00:10:26 +0200

    feature: Add new-project feature.     - Create project-expense form class.     - Create add-project template page for rendering new form to add a project.     - Add project-create view class.     - Add corresponding route at the budget app urlpatterns.     - Update any links to add-project.

--- Testing_ProjectBudgetTracker/budget/urls.py
+++ Testing_ProjectBudgetTracker/budget/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name    = 'budget'
 urlpatterns = [
     path('', views.ProjectsListView.as_view(), name='list'),
+    path('add/', views.ProjectsCreateView.as_view(), name='add'),
 ]
--- Testing_ProjectBudgetTracker/budget/views.py
+++ Testing_ProjectBudgetTracker/budget/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import ListView
-from .models import Project
+from django.views.generic import ListView, CreateView
+from .models import Project, Category
 
 
 class ProjectsListView(ListView):
     model               = Project
     template_name       = 'budget/project-list.html'
     context_object_name = 'project_list'
+
+
+class ProjectsCreateView(CreateView):
+    model         = Project
+    template_name = 'budget/add-project.html'
+    fields        = ['name', 'budget']
+
+    def form_valid(self, form):
+        self.object = form.save()
+        categories  = self.request.POST.get('categoriesString').split(',')
+        for category in categories:
+            Category.objects.create(
+                name=category,
+                project=Project.objects.get(id=self.object.id)
+            )
+        return super().form_valid(form)
