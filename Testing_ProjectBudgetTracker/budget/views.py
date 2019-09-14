77601121b8f4f4c58a496f93c956cc3c2e0de5d3
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-14 21:36:27 +0200

    feature: Add projects-list feature.     - Create base template page for the site.     - Create project-list template page for the budget app.     - Add project-list view class.     - Add corresponding route for it and include the budget app's urls at the site's urlpatterns.

--- Testing_ProjectBudgetTracker/budget/views.py
+++ Testing_ProjectBudgetTracker/budget/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Project
 
-# Create your views here.
+
+class ProjectsListView(ListView):
+    model               = Project
+    template_name       = 'budget/project-list.html'
+    context_object_name = 'project_list'
