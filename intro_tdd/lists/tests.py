bd3795eb7ccd6434978274dd0707ecc0a150ff0c
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-10-24 20:28:06 +0200

    tests: Add basic test case for the home page

--- intro_tdd/lists/tests.py
+++ intro_tdd/lists/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertTrue(response.content.decode().startswith('<html>'))
+        self.assertIn('<title>To-Do Lists</title>', response.content.decode())
+        self.assertTrue(
+            response.content.decode().strip().endswith('</html>'),
+            f"HTML did not end correctly. Was: {response.content}"
+        )
+        self.assertTemplateUsed(
+            response, 'home.html',
+            f'Unexpected template used other than home.html'
+        )
