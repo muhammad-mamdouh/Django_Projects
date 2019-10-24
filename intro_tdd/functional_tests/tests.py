e1d0e12e40cb497f599654dd603fafe4a94db736
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-10-24 18:58:58 +0200

    tests: Add very basic functional tests

---
+++ intro_tdd/functional_tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium import webdriver
+
+
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('functional_tests/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
